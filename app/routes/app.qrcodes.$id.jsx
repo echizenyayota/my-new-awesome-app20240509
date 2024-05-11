ed6c5d43c5f135c506b6041bf01946bc662ca085
echizenyayota <virtualmisso30@gmail.com>
2024-05-11 19:05:07 +0900

    Create a QR code form(Return a JSON Response)

--- app/routes/app.qrcodes.$id.jsx
+++ app/routes/app.qrcodes.$id.jsx
@@ -32,5 +32,14 @@ import { getQRCode, validateQRCode } from "../models/QRCode.server";
 
 export async function loader({ request, params}) {
   const { admin } = await authenticate.admin(request);
-  
+
+  if (params.id === "new") {
+    return json({
+      destination: "product",
+      title: "",
+    });
+  }
+
+  return json(await getQRCode(Number(params.id), admin.graphql));
+
 }
