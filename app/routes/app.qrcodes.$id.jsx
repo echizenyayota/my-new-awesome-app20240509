ab71a81329eec4d614163a7e7a4d017967a9f4d6
echizenyayota <virtualmisso30@gmail.com>
2024-05-11 18:38:01 +0900

    Create a QR code form route(Set up the form route)

---
+++ app/routes/app.qrcodes.$id.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { join, redirect} from "@remix-run/node";
+import {
+  useActionData,
+  useLoaderData,
+  useNavigation, 
+  useSubmit, 
+  useNavigate,
+} from "@remix-run/react";
+import { authenticate } from "../shopify.server";
+import {
+  Card,
+  Bleed,
+  Button,
+  ChoiceList,
+  Divider,
+  EmptyState,
+  InlineStack,
+  InlineError,
+  Layout,
+  Page,
+  Text,
+  TextField,
+  Thumbnail,
+  BlockStack,
+  PageActions,
+} from "@shopify/polaris";
+import { ImageIcon } from "@shopify/polaris-icons";
+
+import db from "../db.server";
+import { getQRCode, validateQRCode } from "../models/QRCode.server";
