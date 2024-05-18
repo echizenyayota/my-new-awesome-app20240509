88eea239a4afd103e0cd90bd10425807882efcf3
echizenyayota <virtualmisso30@gmail.com>
2024-05-18 18:50:37 +0900

    Redirect the customer to the destination URL - Validate the QR code ID

--- app/routes/qrcodes.$id.scan.jsx
+++ app/routes/qrcodes.$id.scan.jsx
@@ -4,4 +4,14 @@ import db from "../db.server";
 
 import { getDestinationUrl } from "../models/QRCode.server";
 
+export const loader = async ({ params }) => {
+  invariant(params.id, "Could not find QR code destination");
+
+  const id = Number(params.id);
+  const qrCode = await db.qRCode.findFirst({ where: { id } });
+
+  invariant(qrCode, "Could not find QR code destination");
+
+
+}
 
