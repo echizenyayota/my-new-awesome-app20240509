523ee1f14535ec237078b410069f41c08e72ec59
echizenyayota <virtualmisso30@gmail.com>
2024-05-18 16:44:28 +0900

    List QR codes - Create index table rows(Warn if a product is deleted)

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -23,47 +23,82 @@ export async function loader({ request }) {
   return json({
     qrCodes,
   });
+}
 
-  const EmptyQRCodeState = ({ onAction }) => (
-    <EmptyState
-      heading="Create unique QR codes for your product"
-      action={{
-        content: "Create QR Code",
-        onAction, 
-      }}
-      image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
-    >
-      <p>Allow customers to scan codes and buy products using their phones</p>
-    </EmptyState>
-  );
+const EmptyQRCodeState = ({ onAction }) => (
+  <EmptyState
+    heading="Create unique QR codes for your product"
+    action={{
+      content: "Create QR Code",
+      onAction, 
+    }}
+    image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+  >
+    <p>Allow customers to scan codes and buy products using their phones</p>
+  </EmptyState>
+);
 
-  function truncate(str, { length = 25 } = {}) {
-    if (!str) return "";
-    if (str.length <= length) return str;
-    return str.slice(0, length) + "...";
-  }
+function truncate(str, { length = 25 } = {}) {
+  if (!str) return "";
+  if (str.length <= length) return str;
+  return str.slice(0, length) + "...";
+}
 
-  const QRTable = ({ qrCodes }) => {
-    <IndexTable
-      resourceName={{
-        singular: "QR code",
-        plural: "QR codes"
-      }}
-      itemCount={qrCodes.length}
-      headings={[
-        { title: "Thumbnail", hidden: true },
-        { title: "Title" },
-        { title: "Product" },
-        { title: "Date created" },
-        { title: "Scans" },
-      ]}
-      selectable={false}
-    >
-      {qrCodes.map((qrCode) => {
-        <QRTableRow key={qrCode.id} qrCode={qrCode} />
-      })}
-    </IndexTable>
-  } 
+const QRTable = ({ qrCodes }) => {
+  <IndexTable
+    resourceName={{
+      singular: "QR code",
+      plural: "QR codes"
+    }}
+    itemCount={qrCodes.length}
+    headings={[
+      { title: "Thumbnail", hidden: true },
+      { title: "Title" },
+      { title: "Product" },
+      { title: "Date created" },
+      { title: "Scans" },
+    ]}
+    selectable={false}
+  >
+    {qrCodes.map((qrCode) => {
+      <QRTableRow key={qrCode.id} qrCode={qrCode} />
+    })}
+  </IndexTable>
+}
+
+const QRTableRow = ({ qrCode }) => {
+  <IndexTable.Row id={qrCode.id} position={qrCode.id}>
+    <IndexTable.Cell>
+      <Thumbnail 
+        source={qrCode.productImage || ImageIcon}
+        alt={qrCode.productTitle}
+        size="small"
+      />
+    </IndexTable.Cell>
+    <IndexTable.Cell>
+      <Link to={`qrcodes/${qrCode.id}`}>
+        {truncate(qrCode.title)}
+      </Link>
+    </IndexTable.Cell>
+    <IndexTable.Cell>
+      {qrCode.productDeleted ? (
+        <InlineStack align="start" gap="200">
+          <span style={{ width: "20px"}}>
+            <Icon source={AlterDiamondIcon} tone="critical" />
+          </span>
+          <Text tone="critical" as="span">
+            Product has been deleted
+          </Text>
+        </InlineStack>
+      ) : (
+        truncate(qrCode.productTitle)
+      )}
+    </IndexTable.Cell>
+    <IndexTable.Cell>
+      {new Date(qrCode.createdAt).toDateString()}
+    </IndexTable.Cell>
+    <IndexTable.Cell>{qrCode.scan}</IndexTable.Cell>
+  </IndexTable.Row>
 }
 
 
