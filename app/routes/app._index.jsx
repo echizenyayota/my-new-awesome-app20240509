1c8dc57f6d44825f825f621cc9803fd41e6c6c1a
echizenyayota <virtualmisso30@gmail.com>
2024-05-20 16:01:20 +0900

    Fix the app._index.jsx

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -13,8 +13,8 @@ import {
   InlineStack,
 } from "@shopify/polaris";
 
-import { getQRCode, getQRCodes } from "../models/QRCode.server";
-import { AlterDiamondIcon, ImageIcon } from "@shopify/polaris-icons";
+import { getQRCodes } from "../models/QRCode.server";
+import { AlertDiamondIcon, ImageIcon } from "@shopify/polaris-icons";
 
 export async function loader({ request }) {
   const { admin, session } = await authenticate.admin(request);
@@ -29,26 +29,26 @@ const EmptyQRCodeState = ({ onAction }) => (
   <EmptyState
     heading="Create unique QR codes for your product"
     action={{
-      content: "Create QR Code",
-      onAction, 
+      content: "Create QR code",
+      onAction,
     }}
     image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
   >
-    <p>Allow customers to scan codes and buy products using their phones</p>
+    <p>Allow customers to scan codes and buy products using their phones.</p>
   </EmptyState>
 );
 
 function truncate(str, { length = 25 } = {}) {
   if (!str) return "";
   if (str.length <= length) return str;
-  return str.slice(0, length) + "...";
+  return str.slice(0, length) + "…";
 }
 
-const QRTable = ({ qrCodes }) => {
+const QRTable = ({ qrCodes }) => (
   <IndexTable
     resourceName={{
       singular: "QR code",
-      plural: "QR codes"
+      plural: "QR codes",
     }}
     itemCount={qrCodes.length}
     headings={[
@@ -60,34 +60,32 @@ const QRTable = ({ qrCodes }) => {
     ]}
     selectable={false}
   >
-    {qrCodes.map((qrCode) => {
+    {qrCodes.map((qrCode) => (
       <QRTableRow key={qrCode.id} qrCode={qrCode} />
-    })}
+    ))}
   </IndexTable>
-}
+);
 
-const QRTableRow = ({ qrCode }) => {
+const QRTableRow = ({ qrCode }) => (
   <IndexTable.Row id={qrCode.id} position={qrCode.id}>
     <IndexTable.Cell>
-      <Thumbnail 
+      <Thumbnail
         source={qrCode.productImage || ImageIcon}
         alt={qrCode.productTitle}
         size="small"
       />
     </IndexTable.Cell>
     <IndexTable.Cell>
-      <Link to={`qrcodes/${qrCode.id}`}>
-        {truncate(qrCode.title)}
-      </Link>
+      <Link to={`qrcodes/${qrCode.id}`}>{truncate(qrCode.title)}</Link>
     </IndexTable.Cell>
     <IndexTable.Cell>
       {qrCode.productDeleted ? (
         <InlineStack align="start" gap="200">
-          <span style={{ width: "20px"}}>
-            <Icon source={AlterDiamondIcon} tone="critical" />
+          <span style={{ width: "20px" }}>
+            <Icon source={AlertDiamondIcon} tone="critical" />
           </span>
           <Text tone="critical" as="span">
-            Product has been deleted
+            product has been deleted
           </Text>
         </InlineStack>
       ) : (
@@ -97,22 +95,19 @@ const QRTableRow = ({ qrCode }) => {
     <IndexTable.Cell>
       {new Date(qrCode.createdAt).toDateString()}
     </IndexTable.Cell>
-    <IndexTable.Cell>{qrCode.scan}</IndexTable.Cell>
+    <IndexTable.Cell>{qrCode.scans}</IndexTable.Cell>
   </IndexTable.Row>
-}
+);
 
 export default function Index() {
   const { qrCodes } = useLoaderData();
   const navigate = useNavigate();
 
-  return(
+  return (
     <Page>
-      <ui-title-bar title="QR Code">
-        <button 
-          variant="primary" 
-          onClick={() => navigate("/app/qrcodes/new")}
-        >
-          Create QR Code
+      <ui-title-bar title="QR codes">
+        <button variant="primary" onClick={() => navigate("/app/qrcodes/new")}>
+          Create QR code
         </button>
       </ui-title-bar>
       <Layout>
@@ -120,7 +115,7 @@ export default function Index() {
           <Card padding="0">
             {qrCodes.length === 0 ? (
               <EmptyQRCodeState onAction={() => navigate("qrcodes/new")} />
-            ) : ( 
+            ) : (
               <QRTable qrCodes={qrCodes} />
             )}
           </Card>
@@ -129,6 +124,3 @@ export default function Index() {
     </Page>
   );
 }
-
-
-
